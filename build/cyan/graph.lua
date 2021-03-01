7ff38c0f2633f24eae6a4301a01bf9a9193bc928
Corey Williamson <euclidianAce@protonmail.com>
2021-02-28 22:03:12 -0600

    graph: add Dag:insert_file

--- build/cyan/graph.lua
+++ build/cyan/graph.lua
@@ -1,10 +1,10 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = true, require('compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local table = _tl_compat and _tl_compat.table or table
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = true, require('compat53.module'); if p then _tl_compat = m end end; local table = _tl_compat and _tl_compat.table or table
 local common = require("cyan.tlcommon")
 local fs = require("cyan.fs")
 local util = require("cyan.util")
 
-
-local values = util.tab.values
+local values, ivalues, keys =
+util.tab.values, util.tab.ivalues, util.tab.keys
 
 local Node = {}
 
@@ -18,6 +18,14 @@ local Node = {}
 
 
 
+local function make_node(input)
+   return {
+      input = input,
+      modules = {},
+      dependents = {},
+   }
+end
+
 local Dag = {}
 
 
@@ -26,7 +34,7 @@ local Dag = {}
 local function mark_for_typecheck(n)
    if n.mark then return end
    n.mark = "typecheck"
-   for _, child in ipairs(n.dependents) do
+   for child in keys(n.dependents) do
       mark_for_typecheck(child)
    end
 end
@@ -34,14 +42,46 @@ end
 local function mark_for_compile(n)
    if n.mark == "compile" then return end
    n.mark = "compile"
-   for _, child in ipairs(n.dependents) do
+   for child in keys(n.dependents) do
       mark_for_typecheck(child)
    end
 end
 
+local function make_dependent_counter()
+   local cache = {}
+   local function count_dependents(n)
+      if cache[n] then return cache[n] end
+      local deps = 0
+      for v in keys(n.dependents) do
+         deps = deps + count_dependents(v) + 1
+      end
+      cache[n] = deps
+      return deps
+   end
+   return count_dependents
+end
+
 function Dag:nodes()
-   local i = self._most_deps
-   local iter = values(self._nodes[i])
+   local count = make_dependent_counter()
+   local most_deps = 0
+   local nodes_by_deps = setmetatable({}, {
+      __index = function(self, key)
+         if key > most_deps then
+            most_deps = key
+         end
+         local arr = {}
+         rawset(self, key, arr)
+         return arr
+      end,
+   })
+   for n in values(self._nodes_by_filename) do
+      table.insert(nodes_by_deps[count(n)], n)
+   end
+
+   setmetatable(nodes_by_deps, nil)
+
+   local i = most_deps
+   local iter = values(nodes_by_deps[i])
    return function()
       local n
       while i >= 0 do
@@ -49,11 +89,15 @@ function Dag:nodes()
          if n then
             return n
          end
-         i = i - 1
-         iter = values(self._nodes[i])
+         repeat i = i - 1
+         until i < 0 or nodes_by_deps[i]
+         if nodes_by_deps[i] then
+            iter = values(nodes_by_deps[i])
+         end
       end
    end
 end
+
 
 function Dag:mark_each(predicate)
    for n in self:nodes() do
@@ -80,61 +124,67 @@ local graph = {
    Dag = Dag,
 }
 
+function graph.empty()
+   return setmetatable({
+      _nodes_by_filename = {},
+   }, { __index = Dag })
+end
+
+
+
+function Dag:insert_file(fstr)
+   local f = type(fstr) == "table" and
+   fstr or
+   fs.path.new(fstr)
+
+   if f:is_absolute() then
+
+      return
+   end
+
+   local real_path = f:to_real_path()
+
+   if self._nodes_by_filename[real_path] then
+
+      return
+   end
+   local _, ext = fs.extension_split(f, 2)
+   if ext ~= ".tl" then
+
+      return
+   end
+   local res = common.parse_file(real_path)
+   if not res then return end
+   local n = make_node(f)
+   self._nodes_by_filename[real_path] = n
+
+   for mod_name in ivalues(res.reqs) do
+
+
+      local search_result = common.search_module(mod_name, true)
+
+      n.modules[mod_name] = search_result
+      self:insert_file(search_result)
+   end
+
+   for node in values(self._nodes_by_filename) do
+      for mod_path in values(node.modules) do
+         local dep_node = self._nodes_by_filename[mod_path:to_real_path()]
+         if dep_node then
+            dep_node.dependents[node] = true
+         end
+      end
+   end
+end
+
 function graph.scan_dir(dir, include, exclude)
-   local nodes_by_filename = {}
-   local d = {}
+   local d = graph.empty()
+
    for p in fs.scan_dir(dir, include, exclude) do
       local _, ext = fs.extension_split(p, 2)
       if ext == ".tl" then
-         local full_p = dir .. p
-         local path = full_p:to_real_path()
-         local res = common.parse_file(path)
-         if res then
-            local require_calls = res.reqs
-            local modules = {}
-            for _, mod_name in ipairs(require_calls) do
-               modules[mod_name] = common.search_module(mod_name, true)
-            end
-            nodes_by_filename[path] = {
-               input = full_p,
-               modules = modules,
-               dependents = {},
-            }
-         end
+         d:insert_file(dir .. p)
       end
-   end
-
-
-   for node in values(nodes_by_filename) do
-      for mod_path in values(node.modules) do
-         local dep_node = nodes_by_filename[mod_path:to_real_path()]
-         if dep_node then
-            table.insert(dep_node.dependents, node)
-         end
-      end
-   end
-
-   local cache = {}
-   local function count_deps(n)
-      if cache[n] then return cache[n] end
-      local deps = 0
-      for _, v in ipairs(n.dependents) do
-         deps = deps + count_deps(v) + 1
-      end
-      cache[n] = deps
-      return deps
-   end
-
-   d._most_deps = 0
-   d._nodes = setmetatable({}, {
-      __index = function(self, key)
-         if key > d._most_deps then d._most_deps = key end
-         rawset(self, key, {})
-         return rawget(self, key)
-      end,
-   })
-   for node in values(nodes_by_filename) do
-      table.insert(d._nodes[count_deps(node)], node)
    end
 
 
--- spec/api/graph_spec.lua
+++ spec/api/graph_spec.lua
@@ -5,19 +5,39 @@ local graph = require("cyan.graph")
 describe("dependency graph builder", function()
    it("should properly count node dependents", function()
       util.do_in(util.write_tmp_dir(finally, {
-         ["foo.tl"] = [[]],
-         ["bar.tl"] = [[require"foo"]],
-         ["baz.tl"] = [[require"bar"]],
+         ["a.tl"] = [[]],
+         ["b.tl"] = [[require"a"]],
+         ["c.tl"] = [[require"b"]],
+         ["d.tl"] = [[require"c"]],
+         ["e.tl"] = [[require"d"]],
+         ["f.tl"] = [[require"e"]],
+         ["g.tl"] = [[require"f"]],
+         ["h.tl"] = [[require"g"]],
+         ["i.tl"] = [[require"h"]],
+         ["j.tl"] = [[require"i"]],
       }), function()
          local g = graph.scan_dir(".")
          local iter = g:nodes()
-         local expected_order = {"foo.tl", "bar.tl", "baz.tl"}
-         local actual_order = {
-            iter().input:to_real_path(),
-            iter().input:to_real_path(),
-            iter().input:to_real_path(),
+
+         local actual_order = {}
+         local iter = g:nodes()
+         for n in iter do
+            table.insert(actual_order, n.input:to_real_path())
+         end
+
+         local expected_order = {
+            "a.tl",
+            "b.tl",
+            "c.tl",
+            "d.tl",
+            "e.tl",
+            "f.tl",
+            "g.tl",
+            "h.tl",
+            "i.tl",
+            "j.tl",
          }
-         assert.is["nil"](iter(), "more than three files found")
+         assert.is["nil"](iter(), "more files found than expected")
          assert.are.same(expected_order, actual_order)
       end)
    end)
