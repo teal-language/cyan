20d47d7e52e0ca829232a549e522956408811fd6
Corey Williamson <euclidianAce@protonmail.com>
2021-06-28 17:55:15 -0500

    Add 'meta' info + version command

--- build/cyan/cli.lua
+++ build/cyan/cli.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local debug = _tl_compat and _tl_compat.debug or debug; local os = _tl_compat and _tl_compat.os or os; local pairs = _tl_compat and _tl_compat.pairs or pairs; local string = _tl_compat and _tl_compat.string or string; local table = _tl_compat and _tl_compat.table or table; local xpcall = _tl_compat and _tl_compat.xpcall or xpcall
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = true, require('compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local debug = _tl_compat and _tl_compat.debug or debug; local os = _tl_compat and _tl_compat.os or os; local pairs = _tl_compat and _tl_compat.pairs or pairs; local string = _tl_compat and _tl_compat.string or string; local table = _tl_compat and _tl_compat.table or table; local xpcall = _tl_compat and _tl_compat.xpcall or xpcall
 
 
 
@@ -67,6 +67,19 @@ parser:flag("-h --help", "Show this help message and exit"):
 action(function()
    os.exit(command.get("help").exec())
 end)
+
+command.new({
+   name = "version",
+   description = [[Print version information and exit]],
+   exec = function()
+      log.info(
+      "Cyan version: ", require("cyan.meta").version, "\n",
+      "Teal version: ", tl.version(), "\n",
+      " Lua version: ", _VERSION)
+
+      return 0
+   end,
+})
 
 require("cyan.commands.initialize")
 require("cyan.commands.check-gen")
---
+++ build/cyan/meta.lua
@@ -0,0 +1,6 @@
+
+
+
+return {
+   version = "0.0.0+dev",
+}
